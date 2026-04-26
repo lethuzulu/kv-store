6b3e74ef6003ef97762188b03656ef7f40f698c7
lethuzulu <lethuzulu456@gmail.com>
2026-04-27 01:15:44 +0200

    feat: add threading and synchronization to TcpServer request handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use kv_store::server::TcpServer;
 
 fn main() {
-    let mut tcp_server = TcpServer::new("localhost:5000").unwrap();
+    let tcp_server = TcpServer::new("localhost:5000").unwrap();
     tcp_server.listen();
 }
--- src/server.rs
+++ src/server.rs
@@ -7,26 +7,32 @@ use anyhow::Result;
 use std::io::Write;
 use std::io::{BufRead, BufReader};
 use std::net::{TcpListener, TcpStream, ToSocketAddrs};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[derive(Debug)]
 pub struct TcpServer {
     inner: TcpListener,
-    store: KvStore,
+    store: Arc<Mutex<KvStore>>,
 }
 
 impl TcpServer {
     pub fn new(address: impl ToSocketAddrs) -> Result<Self> {
         let inner = TcpListener::bind(address)?;
         let store = KvStore::new("store.log")?;
+        let store = Arc::new(Mutex::new(store));
         println!("Tcp Server listening in...");
         Ok(Self { inner, store })
     }
 
-    pub fn listen(&mut self) {
+    pub fn listen(&self) {
         for connection in self.inner.incoming() {
             match connection {
                 Ok(stream) => {
-                    Self::handle_connection(stream, &mut self.store);
+                    let store = Arc::clone(&self.store);
+                    thread::spawn(move || {
+                        handle_connection(stream, store);
+                    });
                 }
                 Err(_) => {
                     println!("Error occurred. Listening for next connection");
@@ -34,60 +40,61 @@ impl TcpServer {
             }
         }
     }
+}
 
-    fn handle_connection(stream: TcpStream, store: &mut KvStore) {
-        let mut line = String::new();
-        let mut writer = stream.try_clone().unwrap();
-        let mut reader = BufReader::new(&stream);
-        loop {
-            match reader.read_line(&mut line) {
-                Ok(0) => break,
-                Ok(_) => {}
-                Err(e) => {
-                    eprintln!("read error: {:?}", e);
-                    break;
-                }
-            }
-            let request = match deserialize_request(&line) {
-                Ok(r) => r,
-                Err(e) => {
-                    eprintln!("deserialization error: {}", e);
-                    let serialized = match serialize_response(Err(e)) {
-                        Ok(v) => v,
-                        Err(e) => {
-                            eprintln!("serialize error: {}", e);
-                            break;
-                        }
-                    };
-                    if let Err(e) = writer.write_all(&serialized) {
-                        eprintln!("write error: {}", e);
-                        break;
-                    }
-                    line.clear();
-                    continue;
-                }
-            };
-
-            let result = handler(request, store); // if this returns an error, we should serialize that error that error must be structured. basically we should take the result as is and pass it along to be serialized
-
-            let serialized_response = match serialize_response(result) {
-                Ok(v) => v,
-                Err(e) => {
-                    eprintln!("serialize error {}", e);
-                    break;
-                }
-            };
-
-            if let Err(e) = writer.write_all(&serialized_response) {
-                eprintln!("write error: {}", e);
+fn handle_connection(stream: TcpStream, store: Arc<Mutex<KvStore>>) {
+    let mut line = String::new();
+    let mut writer = stream.try_clone().unwrap();
+    let mut reader = BufReader::new(&stream);
+    loop {
+        match reader.read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("read error: {:?}", e);
                 break;
             }
-            line.clear();
         }
+        let request = match deserialize_request(&line) {
+            Ok(r) => r,
+            Err(e) => {
+                eprintln!("deserialization error: {}", e);
+                let serialized = match serialize_response(Err(e)) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        eprintln!("serialize error: {}", e);
+                        break;
+                    }
+                };
+                if let Err(e) = writer.write_all(&serialized) {
+                    eprintln!("write error: {}", e);
+                    break;
+                }
+                line.clear();
+                continue;
+            }
+        };
+
+        let result = handler(request, &store);
+
+        let serialized_response = match serialize_response(result) {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("serialize error {}", e);
+                break;
+            }
+        };
+
+        if let Err(e) = writer.write_all(&serialized_response) {
+            eprintln!("write error: {}", e);
+            break;
+        }
+        line.clear();
     }
 }
 
-fn handler(request: Request, store: &mut KvStore) -> Result<Response> {
+fn handler(request: Request, store: &Arc<Mutex<KvStore>>) -> Result<Response> {
+    let mut store = store.lock().unwrap();
     match request.cmd {
         Command::Set { key, value } => {
             store.set(key, value)?;
